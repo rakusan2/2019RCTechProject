6287a0ca91c2faf6dffa436f10154296cdc0aa5b
TomasR <Rakusan2@gmail.com>
2019-01-21 10:13:34 -0500

    Add Comments to wifi.c

--- wifi.c
+++ wifi.c
@@ -34,6 +34,9 @@ int pauseTX=0;
 // Located In Main
 void wifi_receive(unsigned char *data, unsigned int len);
 
+/**
+ * Try to add a byte to the TX buffer
+ */
 void __sendNext() {
     if (!U1STAbits.UTXBF && txPointer < txBufLen && !pauseTX) {
         U1TXREG = txBuf[txPointer];
@@ -48,6 +51,12 @@ void __sendNext() {
     }
 }
 
+/**
+ * Test received data for whether transmission should resume or reset
+ * 
+ * @param data  Received data
+ * @param len   Length of received data
+ */
 void resumeTX(unchar *data,uint len){
     if(startsWith(data,len,"FAIL",2)){
         txBufLen = txPointer = 0;
@@ -57,11 +66,22 @@ void resumeTX(unchar *data,uint len){
     wifi_receive(data,len);
 }
 
+/**
+ * Add a character to the transmission buffer
+ * 
+ * @param c character to be added
+ */
 inline void txBufAddChar(unchar c){
     txBuf[txBufLen] = c;
     txBufLen++;
 }
 
+/**
+ * Add an Array of characters to the TX buffer
+ * 
+ * @param data  Array of characters
+ * @param len   Length of the array
+ */
 void txBufAdd(unchar *data, uint len) {
     uint i;
     for (i = 0; i < len; i++) {
@@ -70,6 +90,11 @@ void txBufAdd(unchar *data, uint len) {
     txBufLen += len;
 }
 
+/**
+ * Add a null terminated Array of characters to the buffer
+ * 
+ * @param data  Array of characters
+ */
 void txBufAdd_(unchar *data){
     uint i=0;
     while(data[i] !='\0'){
@@ -79,33 +104,60 @@ void txBufAdd_(unchar *data){
     txBufLen+=i;
 }
 
+/**
+ * Add an Array of characters to the TX buffer surrounded by quotations
+ * 
+ * @param data  Array of characters
+ * @param len   Length of the array
+ */
 inline void txBufAddStr(unchar *data, uint len){
     txBufAddChar('"');
     txBufAdd(data,len);
     txBufAddChar('"');
 }
+
+/**
+ * Add a null terminated Array of characters to the TX buffer surrounded by quotations
+ * 
+ * @param data  Array of characters
+ */
 inline void txBufAddStr_(unchar *data){
     txBufAddChar('"');
     txBufAdd_(data);
     txBufAddChar('"');
 }
 
+/**
+ * Add an Array of characters to the TX buffer followed by a new line
+ * 
+ * @param data  Array of characters
+ * @param len   Length of the array
+ */
 inline void txBufAddLn(unchar *data, uint len) {
     txBufAdd(data, len);
     txBufAdd("\r\n", 2);
     __sendNext();
 }
+
+/**
+ * Add a null terminated Array of characters to the TX buffer followed by a new line
+ * 
+ * @param data  Array of characters
+ */
 inline void txBufAddLn_(unchar *data) {
     txBufAdd_(data);
     txBufAdd("\r\n", 2);
     __sendNext();
 }
 
+/**
+ * UART Interrupt handler
+ */
 void __ISR(_UART_1_VECTOR, IPL6SOFT) UARTInt() {
     if (IFS1bits.U1RXIF) {
-        while (U1STAbits.URXDA) {
+        while (U1STAbits.URXDA) { // While there are data in the receive buffer
             rxBuf[rxPointer] = U1RXREG;
-            if(pauseTX && rxBuf[rxPointer] == '>'){
+            if(pauseTX && rxBuf[rxPointer] == '>'){ // Resume when READY to transmit TCP DATA
                 pauseTX = 0;
             }
             if (rxBuf[rxPointer] == '\n') {
@@ -117,12 +169,15 @@ void __ISR(_UART_1_VECTOR, IPL6SOFT) UARTInt() {
         }
         IFS1bits.U1RXIF = 0;
     }
-    if (IFS1bits.U1TXIF) {
+    if (IFS1bits.U1TXIF) { // Add new data to the TX buffer after a transmission has happened
         __sendNext();
         IFS1bits.U1TXIF = 0;
     }
 }
 
+/**
+ * Initialize the UART for the WiFi module
+ */
 void wifi_init() {
     int i;
     for (i = 0; i < TXBufSize; i++) {
@@ -144,6 +199,13 @@ void wifi_init() {
     IPC8bits.U1IP = UART1_PRIORITY; // Set Priority Level
 }
 
+/**
+ * Send TCP data
+ * 
+ * @param data      Array of characters to be sent
+ * @param len       Length of the Array
+ * @param linkID    Recipient ID
+ */
 void wifi_send(unchar *data, uint len, unchar linkID) {
     unchar lenSt[4];
     utoa(lenSt, len, 10);
@@ -155,6 +217,11 @@ void wifi_send(unchar *data, uint len, unchar linkID) {
     txBufAddLn(data, len);
 }
 
+/**
+ * Start the TCP server
+ * 
+ * @param port The port used by the server
+ */
 void wifi_startTCPServer(unchar *port) {
     txBufAddLn_("AT+CIPMUX=1"); // TCP Server requires for multiple connections to be enabled
 
@@ -163,6 +230,12 @@ void wifi_startTCPServer(unchar *port) {
     txBufAddLn_(port);
 }
 
+/**
+ * Setup Software AP
+ * 
+ * @param ssid  The SSID of the AP
+ * @param pwd   The password of the AP
+ */
 void wifi_setSoftAP(unchar *ssid, unchar *pwd) {
     txBufAdd_("AT+CWSAP_CUR=");
     txBufAddStr_(ssid);
