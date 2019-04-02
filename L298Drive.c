7df7a9c75a5eb7e0308a79857e4309656d103c7f
Tomas Rakusan <Rakusan2@gmail.com>
2019-04-02 10:39:30 -0400

    Add L298Drive

---
+++ L298Drive.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.c
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+#include <xc.h>
+#include <sys/attribs.h>
+#include "tools.h"
+#include "serializer.h"
+#include "SPIHBridge.h"
+
+#define OC3R_PER_BIT 2
+
+void drive_init(){
+    
+}
---
+++ L298Drive.h
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
